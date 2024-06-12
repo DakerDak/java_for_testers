ff6bcbbcc6f15cd831959ed45d99c55385e32911
Trevor Quinn <dilam-rt@yandex.ru>
2024-06-12 15:28:55 +0300

    добавлена функция equals для сравнения треугольников на  равенства их сторон

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Triangle {
 
     public double side1;
@@ -9,13 +11,30 @@ public class Triangle {
         this.side2 = side2;
         this.side3 = side3;
         if (side1 < 0 || side2 < 0 || side3 < 0) {
-            throw new IllegalArgumentException ("Сторона прямоугольника не  может быть отрицательной");
-           }
-        if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1 ) {
-            throw new IllegalArgumentException ("Cумма двух любых сторон должна быть не меньше третьей стороны");
+            throw new IllegalArgumentException("Сторона прямоугольника не  может быть отрицательной");
+        }
+        if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
+            throw new IllegalArgumentException("Cумма двух любых сторон должна быть не меньше третьей стороны");
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(triangle.side1, this.side1) == 0 && Double.compare(triangle.side2, this.side2) == 0 && Double.compare(triangle.side3, this.side3) == 0)
+                || (Double.compare(triangle.side1, this.side1) == 0 && Double.compare(triangle.side2, this.side3) == 0 && Double.compare(triangle.side3, this.side2) == 0)
+                || (Double.compare(triangle.side1, this.side2) == 0 && Double.compare(triangle.side2, this.side1) == 0 && Double.compare(triangle.side3, this.side3) == 0)
+                || (Double.compare(triangle.side1, this.side2) == 0 && Double.compare(triangle.side2, this.side3) == 0 && Double.compare(triangle.side3, this.side1) == 0)
+                || (Double.compare(triangle.side1, this.side3) == 0 && Double.compare(triangle.side2, this.side1) == 0 && Double.compare(triangle.side3, this.side2) == 0)
+                || (Double.compare(triangle.side1, this.side3) == 0 && Double.compare(triangle.side2, this.side2) == 0 && Double.compare(triangle.side3, this.side1) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(side1, side2, side3);
+    }
 
     public static void main(String[] args) {
 
--- sandbox/src/test/java/TriangleTest.java
+++ sandbox/src/test/java/TriangleTest.java
@@ -4,48 +4,67 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
 
     @Test
-    void canCalculatePerimeter () {
-        var p = new Triangle(3,5,7);
+    void canCalculatePerimeter() {
+        var p = new Triangle(3, 5, 7);
         double result = p.calculateSemiPerimeter();
-        Assertions.assertEquals (7.5, result);
+        Assertions.assertEquals(7.5, result);
     }
 
     @Test
-    void canCalculateArea () {
-        var s = new Triangle(3,5,7);
-        double result = s.calculateAreaTriangle ();
+    void canCalculateArea() {
+        var s = new Triangle(3, 5, 7);
+        double result = s.calculateAreaTriangle();
         Assertions.assertEquals(6.49519052838329, result);
     }
 
     @Test
-    void cannotCreateWithNegativeSide1 () {
+    void cannotCreateWithNegativeSide1() {
         try {
-            new Triangle(-5,9,8);
+            new Triangle(-5, 9, 8);
             Assertions.fail();
-        }
-        catch (IllegalArgumentException exception) {
-
-        }
-    }
-    @Test
-    void cannotCreateWithNegativeSide2 () {
-        try {
-            new Triangle(5,-9,8);
-            Assertions.fail();
-        }
-        catch (IllegalArgumentException exception) {
+        } catch (IllegalArgumentException exception) {
 
         }
     }
 
     @Test
-    void  checkingСorrectnessSidesTriangle () {
+    void cannotCreateWithNegativeSide2() {
+        try {
+            new Triangle(5, -9, 8);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
+
+    @Test
+    void checkingСorrectnessSidesTriangle() {
         try {
             new Triangle(6, 5, 66);
             Assertions.fail();
-        }
-        catch (IllegalArgumentException exception) {
+        } catch (IllegalArgumentException exception) {
 
         }
+    }
+
+    @Test
+    void equalityTriangles() {
+        var t1 = new Triangle(3.0, 5.0, 6.0);
+        var t2 = new Triangle(3.0, 5.0, 6.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void equalityTriangles2() {
+        var t1 = new Triangle(3.0, 5.0, 6.0);
+        var t2 = new Triangle(5.0, 3.0, 6.0);
+        Assertions.assertTrue(t1.equals(t2));
+    }
+
+    @Test
+    void equalityTriangles3() {
+        var t1 = new Triangle(3.0, 5.0, 6.0);
+        var t2 = new Triangle(6.0, 3.0, 5.0);
+        Assertions.assertTrue(t1.equals(t2));
     }
 }
