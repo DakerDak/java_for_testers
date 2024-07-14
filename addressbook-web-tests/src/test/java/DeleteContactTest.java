0d36635365420459902080a39fc91184c710a264
Trevor Quinn <dilam-rt@yandex.ru>
2024-07-14 18:15:52 +0300

    добавлено предусловие для удаления контактов

--- addressbook-web-tests/src/test/java/DeleteContactTest.java
+++ addressbook-web-tests/src/test/java/DeleteContactTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -26,7 +27,27 @@ public class DeleteContactTest {
   @Test
   public void deleteContact() {
     driver.findElement(By.linkText("home")).click();
+    if (!isElementPresent(By.name("selected[]"))) {
+      driver.findElement(By.linkText("add new")).click();
+      driver.findElement(By.name("firstname")).click();
+      driver.findElement(By.name("firstname")).sendKeys("first name");
+      driver.findElement(By.name("middlename")).click();
+      driver.findElement(By.name("middlename")).sendKeys("middle name");
+      driver.findElement(By.name("lastname")).click();
+      driver.findElement(By.name("lastname")).sendKeys("last mane");
+      driver.findElement(By.cssSelector("input:nth-child(75)")).click();
+      driver.findElement(By.linkText("home")).click();
+    }
     driver.findElement(By.name("selected[]")).click();
     driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
+  }
+
+  private boolean isElementPresent (By locator) {
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException exception) {
+      return false;
+    }
   }
 }
