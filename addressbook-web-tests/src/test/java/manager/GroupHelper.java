67940e1ad39c6a471624ce11509d3aca7e6170d9
Trevor Quinn <dilam-rt@yandex.ru>
2024-10-26 21:08:28 +0300

    реализация сравнения список групп при удалении

--- addressbook-web-tests/src/test/java/manager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/manager/GroupHelper.java
@@ -4,6 +4,11 @@ import model.ContactData;
 import model.GroupData;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+
 public class GroupHelper extends HelperBase{
 
     public GroupHelper(ApplicationManager manager) {
@@ -30,15 +35,15 @@ public class GroupHelper extends HelperBase{
         returnToGroupsPage();
     }
 
-    public void removeGroup() {
+    public void removeGroup(GroupData group) {
         openGroupsPage();
-        selectGroup();
+        selectGroup(group);
         removeSelectedGroups();
         returnToGroupsPage();
     }
     public void modifyGroup(GroupData modifiedGroup) {
         openGroupsPage();
-        selectGroup();
+        selectGroup(null);
         initGroupModification();
         fillGroupForm(modifiedGroup);
         submitGroupModification();
@@ -86,8 +91,8 @@ public class GroupHelper extends HelperBase{
 
     }
 
-    private void selectGroup() {
-        click(By.name("selected[]"));
+    private void selectGroup(GroupData group) {
+        click(By.cssSelector(String.format("input[value='%s']", group.id())));
     }
 
     public int getCount() {
@@ -109,4 +114,15 @@ public class GroupHelper extends HelperBase{
     }
 
 
+    public List<GroupData> getList() {
+        var groups = new ArrayList<GroupData>();
+        var spans = manager.driver.findElements(By.cssSelector("span.group"));
+        for (var span : spans) {
+            var name = span.getText();
+            var checkbox = span.findElement(By.name("selected[]"));
+            var id = checkbox.getAttribute("value");
+            groups.add(new GroupData().withId(id).withName(name));
+        }
+        return groups;
+    }
 }
