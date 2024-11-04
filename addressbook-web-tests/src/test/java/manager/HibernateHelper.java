8b33cb25391832138f2d56e66238bcf7999c2830
Trevor Quinn <dilam-rt@yandex.ru>
2024-11-04 22:07:50 +0300

    изменен класс GroupRemovalTests на использованием hbm

--- addressbook-web-tests/src/test/java/manager/HibernateHelper.java
+++ addressbook-web-tests/src/test/java/manager/HibernateHelper.java
@@ -39,12 +39,33 @@ public class HibernateHelper extends HelperBase {
         return new GroupData("" + record.id, record.name, record.header, record.footer);
     }
 
+    private static GroupRecord convert(GroupData data) {
+        var id = data.id();
+        if ("".equals(id)) {
+            id = "0";
+        }
+        return new GroupRecord(Integer.parseInt(id), data.name(), data.header(), data.footer());
+    }
+
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
         }));
     }
+    public long getCroupCount() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("select count (*) from GroupRecord", Long.class).getSingleResult();
+        });
+    }
 
+    public void createGroup(GroupData groupData) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convert(groupData));
+            session.getTransaction().commit();
+        });
+
+    }
 
 
 
@@ -67,4 +88,6 @@ public class HibernateHelper extends HelperBase {
             return session.createQuery("from ContactRecord", ContactRecord.class).list();
         }));
     }
+
+
 }
--- addressbook-web-tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupRemovalTests.java
@@ -10,33 +10,53 @@ import java.util.Random;
 
 public class GroupRemovalTests extends TestBase {
 
-
-
     @Test
-    public void CanRemoveGroup() {
+    public void CanRemoveGroup2() {
 
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("", "", "", ""));
+        if (app.hbm().getCroupCount() == 0) {
+            app.hbm().createGroup(new GroupData("", "", "", ""));
         }
 
-        var oldGroups = app.groups().getList();
+        var oldGroups = app.hbm().getGroupList();
         var rnd = new Random();
         var index = rnd.nextInt(oldGroups.size());
         app.groups().removeGroup(oldGroups.get(index));
-        var newGroups = app.groups().getList();
+        var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.remove(index);
         Assertions.assertEquals(newGroups , expectedList);
 
     }
 
+
+
+
+
+//    @Test
+//    public void CanRemoveGroup() {
+//
+//        if (app.groups().getCount() == 0) {
+//            app.groups().createGroup(new GroupData("", "", "", ""));
+//        }
+//
+//        var oldGroups = app.groups().getList();
+//        var rnd = new Random();
+//        var index = rnd.nextInt(oldGroups.size());
+//        app.groups().removeGroup(oldGroups.get(index));
+//        var newGroups = app.groups().getList();
+//        var expectedList = new ArrayList<>(oldGroups);
+//        expectedList.remove(index);
+//        Assertions.assertEquals(newGroups , expectedList);
+//
+//    }
+
     @Test
     void canRemoveAllGroupsAtOnce() {
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("", "", "", ""));
+        if (app.hbm().getCroupCount() == 0) {
+            app.hbm().createGroup(new GroupData("", "", "", ""));
         }
         app.groups().removeAllGroups();
-        Assertions.assertEquals(0, app.groups().getCount());
+        Assertions.assertEquals(0, app.hbm().getCroupCount());
     }
 
 
