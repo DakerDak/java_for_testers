fe8ccf79b49bd183d09041876c28c7e806785cb0
Trevor Quinn <dilam-rt@yandex.ru>
2024-11-08 23:52:02 +0300

    метод selectAllGroups изменен с использованием функции

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Geometry {
+
+    public static void main(String[] args) {
+
+        Square.printSquareArea(new Square(7.0));
+
+        Rectangle.printRectangleArea(3.0, 5.0);
+    }
+}
