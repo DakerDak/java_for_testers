9ca3699867cb72c4ac2ba4f2e9470c3ab43fb00c
Trevor Quinn <dilam-rt@yandex.ru>
2024-11-09 17:41:17 +0300

    добавила предусловие создание контакта testPhones

--- addressbook-web-tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactInfoTests.java
@@ -1,15 +1,23 @@
 package tests;
 
+import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static manager.ContactHelper.openContactsPage;
+
 public class ContactInfoTests extends TestBase {
 
     @Test
     void testPhones() {
+        if (app.hbm().getContactCount() == 0) {
+            app.hbm().createContact(new ContactData("", "", "", "", "6", "7", "", "", "", ""));
+
+        }
+        openContactsPage();
     var contacts = app.hbm().getContactList();
     var contact = contacts.get(0);
     var phones = app.contacts().getPhones(contact);
